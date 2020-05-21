c871b4fc23d9cf74f6f25291febbf167fad4a3df
descent.teich@gmail.com <descent.teich@gmail.com>
2020-05-21 13:00:52 +0300

    initial commit

--- radixSortCartoon/Form1.Designer.cs
+++ radixSortCartoon/Form1.Designer.cs
@@ -28,13 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.genButton = new System.Windows.Forms.Button();
+            this.sortButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.randArr = new System.Windows.Forms.TextBox();
+            this.sortArr = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // genButton
+            // 
+            this.genButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.genButton.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.genButton.Location = new System.Drawing.Point(42, 225);
+            this.genButton.Name = "genButton";
+            this.genButton.Size = new System.Drawing.Size(178, 37);
+            this.genButton.TabIndex = 2;
+            this.genButton.Text = "Generate";
+            this.genButton.UseVisualStyleBackColor = false;
+            this.genButton.Click += new System.EventHandler(this.genButton_Click);
+            // 
+            // sortButton
+            // 
+            this.sortButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.sortButton.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sortButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.sortButton.Location = new System.Drawing.Point(405, 225);
+            this.sortButton.Name = "sortButton";
+            this.sortButton.Size = new System.Drawing.Size(178, 37);
+            this.sortButton.TabIndex = 3;
+            this.sortButton.Text = "Sort";
+            this.sortButton.UseVisualStyleBackColor = false;
+            this.sortButton.Click += new System.EventHandler(this.sortButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Your random array:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Your sorted array:";
+            // 
+            // randArr
+            // 
+            this.randArr.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.randArr.Location = new System.Drawing.Point(42, 67);
+            this.randArr.Name = "randArr";
+            this.randArr.ReadOnly = true;
+            this.randArr.Size = new System.Drawing.Size(541, 32);
+            this.randArr.TabIndex = 7;
+            // 
+            // sortArr
+            // 
+            this.sortArr.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sortArr.Location = new System.Drawing.Point(42, 164);
+            this.sortArr.Name = "sortArr";
+            this.sortArr.ReadOnly = true;
+            this.sortArr.Size = new System.Drawing.Size(541, 32);
+            this.sortArr.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(654, 350);
+            this.Controls.Add(this.sortArr);
+            this.Controls.Add(this.randArr);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sortButton);
+            this.Controls.Add(this.genButton);
+            this.Name = "Form1";
+            this.Text = "Radix sorting";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button genButton;
+        private System.Windows.Forms.Button sortButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox randArr;
+        private System.Windows.Forms.TextBox sortArr;
     }
 }
 
--- radixSortCartoon/Form1.cs
+++ radixSortCartoon/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Обменная поразрядная сортировка символьного типа данных
+
 namespace radixSortCartoon
 {
     public partial class Form1 : Form
@@ -15,6 +18,68 @@ namespace radixSortCartoon
         public Form1()
         {
             InitializeComponent();
+            sortButton.Enabled = false;
+        }
+        int size;
+        char[] arr;
+
+        private void genButton_Click(object sender, EventArgs e)
+        {
+            sortArr.Clear();
+            randArr.Clear();
+            Random rand = new Random();
+            size = rand.Next(5, 19);
+            arr = new char[size];
+            for (int i = 0; i < size; ++i)
+            {
+                arr[i] = (char)rand.Next(65, 90);
+                randArr.Text += arr[i] + "  ";
+            }
+
+            sortButton.Enabled = true;
+        }
+
+
+        //range -- система счисления (к-во цифр, из которых может состоять код символа, я беру десятичную систему), 
+        //length -- длина наибольшего кода (символы от A до Z, поэтому все коды не превышают 99)
+        //можно было бы взять 16-тиричные коды unicode, но тогда понадобится 16 массивов и итераций будет больше
+        public static void sorting(char[] arr, int range, int maxLength)
+        {
+            ArrayList[] lists = new ArrayList[range];   //здесь хранятся списки элементов (по цифре 0, 1 и тд до 9 в рассматриваемом разряде)
+            for (int i = 0; i < range; ++i)
+                lists[i] = new ArrayList();
+
+            for (int bitNum = 0; bitNum < maxLength; ++bitNum)
+            {
+                //распределение по спискам от 0 до 9
+                for (int i = 0; i < arr.Length; ++i)
+                {
+                    //выделяем цифру на текущей позиции из номера символа и кладем символ в список
+                    int temp = ((int)arr[i] % (int)Math.Pow(range, bitNum + 1)) / (int)Math.Pow(range, bitNum);
+                    lists[temp].Add(arr[i]);
+                }
+                //сборка массива поразрядно
+                int k = 0;
+                for (int i = 0; i < range; ++i)
+                {
+                    for (int j = 0; j < lists[i].Count; ++j)
+                    {
+                        arr[k++] = (char)lists[i][j];
+                    }
+                }
+                for (int i = 0; i < range; ++i)
+                    lists[i].Clear();   //очистка списков элементов по разрядам
+            }
+        }
+
+        private void sortButton_Click(object sender, EventArgs e)
+        {
+            sortArr.Clear();
+            sorting(arr, 10, 2);
+            for (int i = 0; i < size; ++i)
+            {
+                sortArr.Text += arr[i] + "  ";
+            }
         }
     }
 }
